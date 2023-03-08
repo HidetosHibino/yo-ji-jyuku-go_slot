cddf16555eeb3ba533147913c76d0b4843557058
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-03-08 15:59:00 +0900

    FormObjectの適用

--- app/models/basic_yoji.rb
+++ app/models/basic_yoji.rb
@@ -1,6 +1,7 @@
 class BasicYoji < ApplicationRecord
   MOJI_NUM_LENGTH = 4
   VALID_SOUND_CHAR = /\A[ぁ-んー－]+\z/.freeze
+  VALID_NAME_STRING = /\p{Han}{4}/.freeze
 
   include Sampleable
   # has_many :samples, as: :sampleable
@@ -13,7 +14,7 @@ class BasicYoji < ApplicationRecord
   belongs_to :fourth_kanji, class_name: 'Kanji', foreign_key: 'fourth_kanji_id'
 
   validates :sound, format: { with: VALID_SOUND_CHAR, message: :not_sound_hiragana }, allow_blank: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, length: { is: MOJI_NUM_LENGTH }
   # うまくいかないので、nameカラムを追加
   # 弾けるが、First_kanjiがすでに存在します。　になるため
   # validates :first_kanji_id, uniqueness: {
@@ -26,46 +27,4 @@ class BasicYoji < ApplicationRecord
     merge(result)
     # 結果を他のスコープ結果にmergeする感じ
   }
-
-  # form_object にしたほうがいいかもしれない
-  def save_with_kanjis(basic_yoji_param)
-    return false unless check_params(basic_yoji_param)
-    kanjis = basic_yoji_param[:name].chars
-    meaning = basic_yoji_param[:meaning]
-    sound = basic_yoji_param[:sound]
-    ActiveRecord::Base.transaction do
-      self.first_kanji = Kanji.find_or_create_by(letter: kanjis[0])
-      self.second_kanji = Kanji.find_or_create_by(letter: kanjis[1])
-      self.third_kanji = Kanji.find_or_create_by(letter: kanjis[2])
-      self.fourth_kanji = Kanji.find_or_create_by(letter: kanjis[3])
-      self.name = basic_yoji_param[:name]
-      self.meaning = meaning
-      self.sound = sound
-      save!
-    end
-    true
-  rescue StandardError => e
-    logger.debug(e)
-    false
-  end
-
-  def check_params(basic_yoji_param)
-    return_flg = true
-
-    logger.debug(basic_yoji_param[:name].length)
-    unless basic_yoji_param[:name].length == 4
-      errors.add(:base, '四字熟語は4文字で入力してください')
-      return_flg = false
-    end
-
-    # Kanjiモデルのvalidationエラーを拾えないため、ここで実施
-    basic_yoji_param[:name].chars.each do |kanji|
-      unless kanji =~ Kanji::VALID_LETTER_REGEX
-        errors.add(:base, '四字熟語は漢字のみで入力してください')
-        return_flg = false
-        break
-      end
-    end
-    return_flg
-  end
 end
